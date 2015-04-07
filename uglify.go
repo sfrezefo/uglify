41da6fe710eff730543ca085a78eb7f44f5fc9aa
Serge Frezefond <serge.frezefond@gmail.com>
2015-04-07 17:32:14 +0200

    added parameters to command

--- uglify.go
+++ uglify.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"os"
+)
+
+var (
+	inputfile  = flag.String("inputfile", "", "JSON file to be processed")
+	outputfile = flag.String("outputfile", "", "JSON file to be processed")
+	jsonarray  = flag.Bool("jsonarray", false, "suround file with array bracket")
 )
 
 func parsej(f interface{}) {
 	switch vv := f.(type) {
 	case string:
-		fmt.Print('"',vv'"')
+		fmt.Print("\"", vv, "\"")
 	case float64:
 		fmt.Print(vv)
 	case []interface{}:
@@ -29,7 +38,7 @@ func parsej(f interface{}) {
 			if l != 0 {
 				fmt.Print(",")
 			}
-			fmt.Print('"',k,'":')
+			fmt.Print("\"", k, "\":")
 			parsej(v)
 			l++
 		}
@@ -43,10 +52,37 @@ func parsej(f interface{}) {
 	}
 }
 
+func openStdinOrFile(filename string) io.Reader {
+	var err error
+	r := os.Stdin
+	if filename != "" {
+		r, err = os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return r
+}
 func main() {
 	var g interface{}
 
-	file, err := ioutil.ReadFile("test1.json")
+	flag.Parse()
+	infile := openStdinOrFile(*inputfile)
+
+	fmt.Println("inputfile: ", *inputfile)
+	fmt.Println("outputfile: ", *outputfile)
+	fmt.Println("jsonarray: ", *jsonarray)
+	//	if *inputfile == "" {
+	//		in = os.Stdin
+	//	}
+	//	file, err := ioutil.Open
+	//	File("test1.json")
+	if *outputfile == "" {
+	}
+
+	//	file, err := ioutil.ReadFile("test1.json")
+	file, err := ioutil.ReadAll(infile)
+	//file, err := infile.Read()
 	s := string(file)
 	if err != nil {
 		log.Fatal(err)
@@ -57,9 +93,16 @@ func main() {
 	if err1 != nil {
 		log.Fatal(err1)
 	}
+
 	fmt.Println("unmarshalled : ", g)
 	fmt.Println(" ")
+	if *jsonarray == true {
+		fmt.Print("[")
+	}
 	parsej(g)
+	if *jsonarray == true {
+		fmt.Print("]")
+	}
 	fmt.Println(" ")
 
 }
